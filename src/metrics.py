7346f967bf151b3700a08ae3095e6826d1e1f32b
kchardon <katiachardon@outlook.fr>
2024-02-29 23:37:59 +0100

    skeleton of metrics file

---
+++ src/metrics.py
@@ -0,0 +1,30 @@
+
+
+# Utility functions
+def transcripts_audio(outputs, predicitions):
+    # Returns transcripts of outputs and predicted outputs
+    # TO DO
+
+    return
+
+# Metrics on transcripts
+def compute_BLEU(outputs, predicitions):
+    # TO DO
+
+    return
+    
+def compute_chrf(outputs, predicitions):
+    # TO DO
+
+    return
+    
+def compute_charBLEU(outputs, predicitions):
+    # TO DO
+
+    return
+    
+# Metrics on audios
+def compute_MCD(outputs, predicitions):
+    # TO DO
+
+    return
