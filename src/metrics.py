4cccd3367f4763302f0ff0fc01739b4c153199e3
kchardon <katiachardon@outlook.fr>
2024-03-01 10:02:35 +0100

    beginning of transcirpts_audio function

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,15 @@
-
+import torch
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
 
 # Utility functions
 def transcripts_audio(outputs, predicitions):
     # Returns transcripts of outputs and predicted outputs
-    # TO DO
+    processor = WhisperProcessor.from_pretrained("openai/whisper-base")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
+    # For now we translate to english
+    model.config.forced_decoder_ids = WhisperProcessor.get_decoder_prompt_ids(language="english", task="transcribe")
+
+    input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features
 
     return
 
