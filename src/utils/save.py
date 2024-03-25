d62a323a45ff6cf020a7c30d43940345ab81b01b
kchardon <katiachardon@outlook.fr>
2024-03-25 14:06:50 +0100

    update save function

--- src/utils/save.py
+++ src/utils/save.py
@@ -2,9 +2,9 @@ import scipy
 
 
 def save_wav(out_dir, out_name, rate, data):
-    scipy.io.wavfile.write(out_dir + out_name, rate=rate, data=data)
+    scipy.io.wavfile.write(out_dir + '/' + out_name, rate=rate, data=data)
 
 
 def save_batch(out_dir, rate, batch):
     for sample in batch:
-        save_wav(out_dir, "test.wav", rate, sample)
+        save_wav(out_dir, "out.wav", rate, sample)
