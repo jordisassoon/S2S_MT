5ae114c089b5001c5d47fbd1b7017e6cd3a8ed93
kchardon <katiachardon@outlook.fr>
2024-03-27 19:17:31 +0100

    max_size of dataset + common_voice in local

--- src/main.py
+++ src/main.py
@@ -7,36 +7,37 @@ from models.MT.AutoMT import AutoMT
 from models.TTS.VITS import VITS
 
 from utils.metrics import compute_metrics, compute_all_text_metrics
-from utils.save import save_wav
+from utils.save import save_batch
 
 from tqdm import tqdm
 import librosa
-import torch
-from torch.utils.data import DataLoader
-import numpy as np
+import pandas as pd
 
 from token_hf import token
 
 
 def sample_audio(audio):
-    speech_array, _ = librosa.load(audio["file"], sr=16000) # model's sampling rate
+    speech_array, _ = librosa.load(audio["audio"]["path"], sr=16000) # model's sampling rate
     return speech_array
 
 
 def process_batches(S2T, MT, T2S, sampling_rate, args, batch_size=5):
     # Load datasets
-    # Target, English
+    # Source, English
     cvss = load_dataset(
         "google/cvss", "cvss_c", languages=["fr"], split="validation", trust_remote_code=True
     )
     
-    # Source, French
+    # Target, French
     common_voice = load_dataset(
-        "mozilla-foundation/common_voice_4_0", "fr", split="validation", trust_remote_code=True, streaming=True, token=token
+        "mozilla-foundation/common_voice_4_0", "fr", split="validation", trust_remote_code=True, token=token, cache_dir='/data/kchardon-22/datasets'
     )
+    max_size = args.max_size
+    if max_size is None or max_size > len(cvss) :
+        max_size = len(cvss)
 
-    if len(cvss) < batch_size:
-        batch_size = len(cvss)
+    if max_size < batch_size:
+        batch_size = max_size
 
     # Scores over all the samples
     bleu_all = 0
@@ -44,36 +45,39 @@ def process_batches(S2T, MT, T2S, sampling_rate, args, batch_size=5):
     chrf_all = 0
     mcd_all = 0
 
-    target = iter(common_voice)
-    count = 0
-    for b in tqdm(range(400)):
-        # Iterate through Common Voice
-        s = next(target, 'END')
-        if s == 'END':
-            break
-        
-        source = cvss[b]
-        source_audio = np.array([sample_audio(source)])
-        source_text = np.array([source['text']])
+    columns = ['file_name','source_text','extracted_text', 'S2T_BLEU', 'S2T_CHARBLEU', 'target_text', 'translated_text', 'MT_BLEU', 'MT_CHARBLEU']
+    steps_scores = pd.DataFrame(columns=columns)
 
-        target_audio = np.array([librosa.resample(y = s['audio']['array'], orig_sr = 48000, target_sr = sampling_rate)])[0] # Common Voice's sr to model's sr
-        target_text = np.array([s['sentence'].lower()])
+    columns = ['file_name', 'target_text', 'transcribed_target', 'bleu', 'charbleu', 'chrf', 'mcd']
+    s2s_scores = pd.DataFrame(columns=columns)
+
+    for b, i in tqdm(enumerate(range(0, max_size, batch_size))):
+        cvss_batch = [cvss[index] for index in range(i, i + batch_size)]
+        cv_batch = [common_voice[index] for index in range(i, i + batch_size)]
+        
+        # Source, English
+        source_audio = [sample_audio(source) for source in cvss_batch]
+        source_text = [source['text'] for source in cvss_batch]
+
+        # Target, French
+        target_audio = [sample_audio(target) for target in cv_batch]
+        target_text = [target['sentence'].lower() for target in cv_batch]
 
         # Speech to text
         extracted_text = S2T(source_audio, sampling_rate)
         # Metrics on text between source_text and extracted_text
-        # bleu, charbleu, chrf = compute_all_text_metrics(source_text, extracted_text)
+        # bleu, charbleu, _ = compute_all_text_metrics(source_text, extracted_text)
 
         # Machine translation
         translated_text = MT(extracted_text)
         # Metrics on text between translated_text and target_text
-        # bleu, charbleu, chrf = compute_all_text_metrics(target_text, translated_text)
+        # bleu, charbleu, _ = compute_all_text_metrics(target_text, translated_text)
 
         # Text to speech
-        translated_audio = T2S(translated_text).detach().cpu().numpy()[0]
+        translated_audio = T2S(translated_text).detach().cpu().numpy()
 
         # Save audio translations
-        save_wav(out_dir=args.out_dir, out_name=s['audio']['path'][6:], rate=sampling_rate, data=translated_audio)
+        save_batch(out_dir=args.out_dir, out_name=[source['id'] for source in cvss_batch], rate=sampling_rate, data=translated_audio)
 
         # Compute all the metrics for translated_audio
         bleu, charbleu, chrf, mcd = compute_metrics(
@@ -93,7 +97,8 @@ def process_batches(S2T, MT, T2S, sampling_rate, args, batch_size=5):
         chrf_all += chrf
         mcd_all += mcd
 
-        count += 1
+        count += batch_size
+        break
     
     bleu_all /= count
     charbleu_all /= count
@@ -170,6 +175,11 @@ if __name__ == "__main__":
         type=str,
         metavar="DIR",
         help="path where the audios will be saved",
+    )
+    parser.add_argument(
+        "--max_size",
+        type=int,
+        help="number of samples to process",
     )
     _args = parser.parse_args()
     main(_args)
